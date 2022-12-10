00a85d4afa20aa5adef36a8d1374babc6009e81b
Harsh <valambeharsh1701@gmail.com>
2022-12-10 15:42:05 +0530

    new

---
+++ leetcode/3sumclosest.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length; 
+        int res = 0; 
+        if (n <= 3) {
+            for (int num:nums) 
+                res += num; 
+            return res; 
+        }
+        
+        res = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i <= n - 3; i++) {
+            int j = i + 1; 
+            int k = n - 1; 
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (Math.abs(target - res) >= Math.abs(target - sum)) {
+                    res = sum; 
+                    if (res == target) return res; 
+                }
+                if (sum > target) k--; 
+                else if (sum < target) j++; 
+            }
+        }
+        return res; 
+    }
+}
